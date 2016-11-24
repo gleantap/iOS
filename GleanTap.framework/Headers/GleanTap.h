92b8106aa61e632ce5ed30a7d133c8083aefb3d5
Malini Ram <malini.ramanathan@snyxius.com>
2016-11-24 18:06:44 +0530

    First Commit

---
+++ GleanTap.framework/Headers/GleanTap.h
@@ -0,0 +1,19 @@
+//
+//  GleanTap.h
+//  GleanTap
+//
+//  Created by Snyxius on 11/23/16.
+//  Copyright © 2016 snyxius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GleanTap.
+FOUNDATION_EXPORT double GleanTapVersionNumber;
+
+//! Project version string for GleanTap.
+FOUNDATION_EXPORT const unsigned char GleanTapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GleanTap/PublicHeader.h>
+
+#import <GleanTap/GleanTapImplementation.h>
---
+++ GleanTap.framework/Headers/GleanTapImplementation.h
@@ -0,0 +1,16 @@
+//
+//  GleanTapImplementation.h
+//  GleanTap
+//
+//  Created by Snyxius on 11/23/16.
+//  Copyright © 2016 snyxius. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface GleanTapImplementation : NSObject
+-(void)myFirstFunction:(NSString*)string;
+-(void)mySecondFunc:(NSString*)string;
+-(void)myThirdFunc;
+@end
