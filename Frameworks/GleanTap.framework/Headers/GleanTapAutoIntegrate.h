f10f5120780055f707b378edef84a71d43c2cc98
Malini Ram <malini.ramanathan@snyxius.com>
2017-01-10 19:10:39 +0530

    Jan10

---
+++ Frameworks/GleanTap.framework/Headers/GleanTapAutoIntegrate.h
@@ -0,0 +1,27 @@
+//
+//  GleanTapAutoIntegrate.h
+//  GleanTap
+//
+//  Created by Snyxius on 11/23/16.
+//  Copyright © 2016 snyxius. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <UserNotifications/UserNotifications.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface GleanTapAutoIntegrate : NSObject<UNUserNotificationCenterDelegate,CLLocationManagerDelegate,UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+-(void)registerForPushNotifications;
+-(void)handlePushNotificationRegistration:(NSString*)deviceToken;
+-(void)handlePushNotificationRegistration:(NSString*)deviceToken withDataDict:(NSDictionary*)dataDict;
+-(void)saveUserDetails:(NSDictionary*)dataDict;
+-(void)handlePushReceived:(NSDictionary*)pushData;
+-(void)triggerEventWithEventName:(NSString*)eventName;
+-(void)triggerTags:(NSArray*)tagNames;
+-(void)identitfyUserWithData:(NSDictionary*)dataDict;
++ (GleanTapAutoIntegrate*)sharedObject;
+- (BOOL) autoIntegrate;
+@end
--- GleanTapFramework.podspec
+++ GleanTapFramework.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "GleanTapFramework"
-  s.version      = "1.0.15"
+  s.version      = "1.0.16"
   s.summary      = "A short description of GleanTapFramework."
 
   # This description is used to generate tags and improve search results.
